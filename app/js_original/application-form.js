3c78e8f45d85d19a310ff33917040d0f4154fb57
ArtRIFF <panasuck@bigmir.net>
2021-07-15 23:16:01 +0300

    add validate to application-form

--- app/js_original/application-form.js
+++ app/js_original/application-form.js
@@ -1,34 +1,66 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-  let xhr = new XMLHttpRequest(),
-    applicationForm = document.querySelector('.application__form'),
-    btnSend = document.querySelector('.application__button');
+  let applicationForm = document.querySelector('.application__form');
  
-
-  btnSend.addEventListener("click", () => {
-      xhr.open("POST", "mail.php", true);
-      let formData = new FormData(applicationForm);
-      xhr.addEventListener('load', () => {
-        if (xhr.readyState == 4) {
-          switch (xhr.status) {
-            case 200:
-              console.log("We got succeed request");
-              applicationForm.reset();
-              break;
-            case 301:
-              console.log("Our request moved permanently");
-              break;
-            case 404:
-              console.log("We don't find anything!!!");
-              break;
-            default:
-              console.log("Error!!!");
-              break;
-          }
-        };
-      });
-      xhr.send(formData);
+    new JustValidate('.application__form', {
+      rules: {
+        name: {
+          required: true,
+          minLength: 2
+        },
+        phone: {
+          required: true
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        textarea: {
+          required: true,
+          maxLength: 150
+        }
+      },
+      messages: {
+        name: {
+          required: "Введите имя!"
+      
+        },
+        email: {
+          required: "Введите email!"
+        },
+        phone: {
+          required: "Введите номер телефона!"
+        },
+        textarea: {
+          required: "Вы забыли написать сообщение",
+          maxLength: "В кратце пожалуйста..."
+        }
+      },
+      submitHandler: function (applicationForm) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "mail.php", true);
+        let formData = new FormData(applicationForm);
+        xhr.addEventListener('load', () => {
+          if (xhr.readyState == 4) {
+            switch (xhr.status) {
+              case 200:
+                console.log("We got succeed request");
+                applicationForm.reset();
+                break;
+              case 301:
+                console.log("Our request moved permanently");
+                break;
+              case 404:
+                console.log("We don't find anything!!!");
+                break;
+              default:
+                console.log("Error!!!");
+                break;
+            }
+          };
+        });
+        xhr.send(formData);
+      }
+    });
   
-    
-  });
 });
